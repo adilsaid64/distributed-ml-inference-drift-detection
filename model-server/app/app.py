6cc11a74b47fae328416cb27e7ef7cf7d7b02075
adil <adilsaid64@gmail.com>
2025-07-10 23:54:40 +0100

    initial commit

---
+++ model-server/app/app.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, BackgroundTasks
+from fastapi.responses import Response
+from prometheus_client import Counter, Histogram, Summary, generate_latest
+from pydantic import BaseModel
+
+app = FastAPI(title="Model-Server", description="Model Server Endpoint")
+
+
+class GetPredictionRequest(BaseModel):
+    """Get Prediction Request Schema"""
+    ...
+
+
+class GetPredictionResponse(BaseModel):
+    """Get Prediction Response Schema"""
+    ...
+
+
+def send_data_to_metric_server(data: ...) -> None:
+   """Sends data to the metric server"""
+   ...
+
+@app.post("/get-prediction/")
+async def ingest(request: GetPredictionRequest, background_tasks: BackgroundTasks) -> GetPredictionResponse:
+    """Ingest a text passage into the RAG system and store it in LakeFS"""
+    
+    # send data as a background task to metric server 
+    background_tasks.add_task(send_data_to_metric_server, ...)
+
+    # do predictions
+    model = ...
+    pred = ...
+
+    return pred
+
+
+
+@app.get("/metrics")
+def metrics():
+    """..."""
+    return Response(generate_latest(), media_type="text/plain")
