f98e7499a0aca3fef9c49777ead3141a50fd359b
adil <adilsaid64@gmail.com>
2025-07-27 15:19:14 +0100

    added locustfile testing load capacity

---
+++ locustfile.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from locust import HttpUser, task, between
+
+MODEL_SERVER_URL = "http://localhost:8001/get-prediction"
+REQUEST_INTERVAL = 1.0
+
+X, y = load_breast_cancer(as_frame=True, return_X_y=True)
+
+class ClientTestUser(HttpUser):
+
+    @task
+    def get_prediction(self):
+        sample = X.sample(1)
+
+        if np.random.random() < 0.1:
+            noise_std = 100
+            sample += noise_std * np.random.randn(*sample.shape)
+
+        payload = {
+            "features": {
+                "columns": sample.columns.tolist(),
+                "data": sample.values.tolist()
+            }
+        }
+        self.client.post(MODEL_SERVER_URL, json=payload)
